69ce6922124450089298372dcbdae315f39a2215
vintch <uq.vintch@gmail.com>
2018-01-22 17:31:31 +0200

    ES2 support update: removed SAMPLER_2D_RECT, removed unnecessary glEnable()

--- pyshaders_extensions/sampler2D_uniforms.py
+++ pyshaders_extensions/sampler2D_uniforms.py
@@ -1,8 +1,8 @@
-from rpi_gles2_native import glUniform1iv, glActiveTexture, glEnable, glBindTexture, \
-    GLint, GL_TEXTURE_2D, GL_TEXTURE0, GL_SAMPLER_2D, GL_SAMPLER_2D_RECT
+from rpi_gles2_native import glUniform1iv, glActiveTexture, GLint, glBindTexture, \
+    GL_TEXTURE0, GL_SAMPLER_2D
 
 
-def uniform_sampler2D_setter_impl(loc, count, get_data_ptr_func, value, accessor, rectangle):
+def uniform_sampler2D_setter(loc, count, get_data_ptr_func, value, accessor):
     if "texture_unit_bindings" not in accessor.__extensions_storage__:
         accessor.__extensions_storage__["texture_unit_bindings"] = dict([(-1, -1)])  # max + 1 == 0
     bindings = accessor.__extensions_storage__["texture_unit_bindings"]
@@ -10,27 +10,17 @@ def uniform_sampler2D_setter_impl(loc, count, get_data_ptr_func, value, accessor
     if type(value) is int:
         texture_unit = value
     elif hasattr(value, "target") and hasattr(value, "id"):
-        if (value.target != GL_TEXTURE_2D) ^ rectangle:
-            raise ValueError("Texture expected to be {} rectangle".format("a" if rectangle else "not a"))
         if loc_str not in bindings:
             texture_unit = max(bindings.values()) + 1
             bindings[loc_str] = texture_unit
         else:
             texture_unit = bindings[loc_str]
-        glActiveTexture(GL_TEXTURE0 + texture_unit)
-        glEnable(value.target)
-        glBindTexture(value.target, value.id)
     else:
         raise TypeError("Sampler2D uniform got value of unsupported type")
+    glActiveTexture(GL_TEXTURE0 + texture_unit)
+    if type(value) is not int:
+        glBindTexture(value.target, value.id)
     glUniform1iv(loc, count, get_data_ptr_func(texture_unit))
-
-
-def uniform_sampler2D_setter(loc, count, get_data_ptr_func, value, accessor):
-    uniform_sampler2D_setter_impl(loc, count, get_data_ptr_func, value, accessor, False)
-
-
-def uniform_sampler2DRect_setter(loc, count, get_data_ptr_func, value, accessor):
-    uniform_sampler2D_setter_impl(loc, count, get_data_ptr_func, value, accessor, True)
 
 
 def supported():
@@ -40,7 +30,6 @@ def supported():
 def load(mod):
     # Add uniform data
     uniform_data = {
-        GL_SAMPLER_2D:      (GLint, 1, uniform_sampler2D_setter),
-        GL_SAMPLER_2D_RECT: (GLint, 1, uniform_sampler2DRect_setter)
+        GL_SAMPLER_2D:      (GLint, 1, uniform_sampler2D_setter)
     }
     mod.UNIFORMS_DATA.update(uniform_data)
